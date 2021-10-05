04e2163df3cf96420f0cea008cb17a57639ee64b
Pratik Suthar <pratik@cypherincorporated.org.in>
2021-10-05 12:14:13 +0530

    new changes

---
+++ save.php
@@ -0,0 +1,62 @@
+<?php
+include 'database.php';
+
+if(count($_POST)>0){
+	if($_POST['type']==1){
+		$name=$_POST['name'];
+		$number=$_POST['number'];
+		$sql = "INSERT INTO `reg1`( `name`, `number`) 
+		VALUES ('$name','$number')";
+		if (mysqli_query($conn, $sql)) {
+			echo json_encode(array("statusCode"=>200));
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+if(count($_POST)>0){
+	if($_POST['type']==2){
+		$id=$_POST['id'];
+		$name=$_POST['name'];
+		$number=$_POST['number'];
+		
+		$sql = "UPDATE `reg1` SET `name`='$name',`number`='$number' WHERE id=$id";
+		if (mysqli_query($conn, $sql)) {
+			echo json_encode(array("statusCode"=>200));
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+if(count($_POST)>0){
+	if($_POST['type']==3){
+		$id=$_POST['id'];
+		$sql = "DELETE FROM `reg1` WHERE id=$id ";
+		if (mysqli_query($conn, $sql)) {
+			echo $id;
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+if(count($_POST)>0){
+	if($_POST['type']==4){
+		$id=$_POST['id'];
+		$sql = "DELETE FROM reg1 WHERE id in ($id)";
+		if (mysqli_query($conn, $sql)) {
+			echo $id;
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+
+?>
